0e309ed7564d721b5d2fd4cbb7ea572cf4fc508c
Flavia Degelo Camilo <flaviadegelocamilo@MacBook-Pro-de-Flavia.local>
2017-11-17 14:36:08 -0200

    programa funcionando

--- src/main.c
+++ src/main.c
@@ -3,12 +3,49 @@
 int main() {
   int a[5];
   int n;
+  int c[5];
+  int flag;
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
+ c[0]=a[0];
+ c[1]=a[1];
+ c[2]=a[2];
+ c[3]=a[3];
+ c[4]=a[4];
+
+
   for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
+
+    flag=1;
+
+    for(int i=0; i<5; i++){
+
+
+
+        if(c[i]==1){
+
+            printf("%d", i);
+            c[i]=a[i];
+            flag=0;
+        }
+
+        else{
+            c[i]--;
+        }
+
+
+    }
+
+    if(flag==1){
+
+        printf("-");
+    }
+    printf("\n");
+
   }
+
 
   return 0;
 }
+
